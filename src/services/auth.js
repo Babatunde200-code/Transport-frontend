1b78698c0c0614cbfd899aa62d8d90a4a4940c95
Payourse <payourse@Payourses-MacBook-Pro.local>
2025-07-28 04:50:23 +0100

    my new commit

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,2 +1,7 @@
 // services/auth.js
 export const getAuthToken = () => localStorage.getItem('access_token');
+export const getUserInfo = () => {
+    const user = localStorage.getItem("user");
+    return user ? JSON.parse(user) : null;
+  };
+  
