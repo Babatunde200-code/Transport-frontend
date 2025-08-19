50f79f9f50700a8323e54d232f5cf28b7dfec5f4
Payourse <payourse@Payourses-MacBook-Pro.local>
2025-08-19 22:40:31 +0100

    new commit

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -29,7 +29,7 @@ const Signup = () => {
     setSuccess('');
   
     try {
-      const res = await fetch('https://transport-2-0imo.onrender.com/api/signup/', {
+      const res = await fetch('https://transport-2-0imo.onrender.com/signup/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
