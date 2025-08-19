956b79c4ac58594f5bb3d3b21fa752705214d915
Payourse <payourse@Payourses-MacBook-Pro.local>
2025-08-19 16:52:38 +0100

    new commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/login/', {
+      const response = await fetch('https://transport-2-0imo.onrender.com/api/login/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -24,7 +24,7 @@ const Signup = () => {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const res = await fetch('http://127.0.0.1:8000/api/signup/', {
+      const res = await fetch('https://transport-2-0imo.onrender.com/api/signup/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
